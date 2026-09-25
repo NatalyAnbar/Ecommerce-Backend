321b902fd4e59f9ec362620aa4b56f4e744fbc85
Nataly <natalyalanbar7@gmail.com>
2026-09-25 23:41:18 +0300

    feat(store): split and serialize list and create product flows

--- store/views.py
+++ store/views.py
@@ -1,19 +1,25 @@
 from rest_framework.viewsets import ModelViewSet
-from . import models,serializer
+from . import models,Serializers
 
 
 class CategoryView(ModelViewSet):
+
+    # Fetch all categories
     queryset = models.Category.objects.all()
-    serializer_class = serializer.CategorySerializer
+    serializer_class = Serializers.CategorySerializer
     lookup_field = 'slug'
 
 
 class ProductView(ModelViewSet):
+
+    # Fetch all products
     queryset = models.Product.objects.all()
-    serializer_class = serializer.ProductSerializer
+    serializer_class = Serializers.ProductSerializer
     lookup_field = 'slug'
 
 
 class ProductImageView(ModelViewSet):
+
+    # view and manage product gallery images
     queryset = models.ProductImage.objects.all()
-    serializer_class = serializer.ImageSerializer
+    serializer_class = Serializers.ImageSerializer
